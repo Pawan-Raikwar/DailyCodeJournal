dfaf041cbe0686dd2bc5adb0c63dede25008eb9f
Pawan Raikwar <pawanraikwar614@gmail.com>
2025-10-29 15:56:22 +0530

    Day6

---
+++ GenralPracties/practies2.java
@@ -0,0 +1,18 @@
+package GenralPracties;
+import java.util.HashSet;
+public class practies2 {
+
+    public static void main(String[] args) {
+
+        HashSet<Integer>hs=new HashSet<>();
+
+        hs.add(1);
+        hs.add(2);
+        hs.add(3);
+        hs.add(5);
+        hs.add(4);
+
+        System.out.println(hs);
+
+    }
+}
