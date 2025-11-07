50874396e8dc69bf847b345dee76be39174d3cbe
Pawan Raikwar <pawanraikwar614@gmail.com>
2025-11-07 20:44:35 +0530

    OOPS

---
+++ GenralPracties/OOPS.java
@@ -0,0 +1,15 @@
+package GenralPracties;
+
+public class OOPS {
+
+    class Animal{
+        public void animal1(){
+            System.out.println("This is animal method");
+        }
+    }
+
+    public  void main(String[] args) {
+        Animal animal=new Animal();
+        animal.animal1();
+    }
+}
