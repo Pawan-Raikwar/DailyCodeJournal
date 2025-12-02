7bf422da8c1e726164caed4b6aa5634d5feafa6e
Pawan Raikwar <pawanraikwar614@gmail.com>
2025-12-02 20:45:21 +0530

    Climbing Stairs

---
+++ GenralPracties/ClimbingStairs.java
@@ -0,0 +1,17 @@
+package GenralPracties;
+
+// leetcode 70
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        int n=5;
+        int dp[]=new int[n+1];
+
+        dp[0]=1;
+        dp[1]=1;
+
+        for(int i=2;i<n+1;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        System.out.println("The ans is "+dp[n]);
+    }
+}
